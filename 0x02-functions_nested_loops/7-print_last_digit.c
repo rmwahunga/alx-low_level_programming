db6e446969f6478f51531a8a874d8f765fb0b077
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-08 02:37:35 -0700

    fixed code

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_last_digit(int nld)
+{
+	int pld;
+
+	pld = (nld % 10);
+
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+
+		_putchar(pld + '0');
+		return (pld);
+}
