18ef5b6702ec2c4664671330a6e566aad25a981c
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-09 00:44:24 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int sp, ro, tr;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (ro = 0; ro <= (size - 1); ro++)
+		{
+			for (sp = 0; sp < (size - 1) - ro; sp++)
+			{
+				_putchar(' ');
+			for (tr = 0; tr <= ro; tr++)
+			{
+				_putchar('#');
+																								}
+			_putchar('\n');
+		}
+		
+																						}
+}
