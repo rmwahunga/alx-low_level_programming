3db744b7717c80b6e25221c72167b790abb4cde2
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-12 04:22:18 -0700

    half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,43 +1,30 @@
 #include "main.h"
+
+#include <stdio.h>
+#include <string.h>
 /**
- * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
- * @str: input
- * Return: half of input
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
- *  * puts_half - prints half of a string, followed by a new line
- *   * @str: string to print
- *    */
-
+ * * puts_half - prints half string
+ * *
+ * *@str: string
+ * * Return: void
+ * */
 void puts_half(char *str)
 {
-	int idx;
-	int len = _strlen(str);
-
-	/* find the index to start depending on even/odd amount of strlen */
-	if (len % 2 != 0)
-		idx = (len / 2) + 1;
-	else
-		idx = (len / 2);
-
-	while (idx < len)
+	int n = strlen(str);
+	int i;
+	if (n % 2 == 0)
 	{
-		_putchar(*(str + idx));
-		idx++;
+		for (i = 0; i < n / 2; i++)
+		{
+																		printf("%c", str[i]);
+																	}
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = n / 2; i < n; i++)	
+		{
+																		printf("%c", str[i]);
+																	}
+	}
+	printf("\n");
 }
