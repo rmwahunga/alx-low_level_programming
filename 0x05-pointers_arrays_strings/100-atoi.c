9e2ac7bcf55ee80eb811fa5f8b9620bab37e7c37
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-12 11:04:35 -0700

    string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,17 +17,21 @@ int _atoi(char *s)
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-		minus *= -1;
+		{
+		       	minus *= -1;
 
 															if (s[i] >= '0' && s[i] <= '9')
-		{
+			{
 			result *= 10;
 			result -= (s[i] - '0');
 			boolean = 1;
-		}
-															else if (boolean ==1)
+			}
+														}
+		else if (boolean == 1)
+		{
 																break;
-																i++;
+				i++;
+		}
 	}
 	result *= minus;
 	return (result);
