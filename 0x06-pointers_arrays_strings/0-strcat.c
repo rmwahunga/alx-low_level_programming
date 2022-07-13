47c6e74a987deb09f7453caba181d1aec07a4b6b
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-13 03:16:24 -0700

    terminating null byte

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, ch c)
+     	int i;
+{	
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+                     return (s + i);
+        } 
+
+	return (NULL);
+}
