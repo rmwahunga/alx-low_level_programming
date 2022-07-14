c75f35a7456f403ff3d8618bb98e9df46142cf74
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-14 09:19:07 -0700

    jsckbauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * jack_bauer - print every minute of the day
  *
- * Return: Always 0.
+ * Return: void
  */
 void jack_bauer(void)
 {
-	int h, m;
+	int hour, min;
 
-	for (h = 0; h < 24; h++)
+	hour = 0;
+	while (hour < 24)
 	{
-		for (m = 0; m < 60; m++)
+		min = 0;
+		while (min < 60)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
 			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
 			_putchar('\n');
 			min++;
-		}
+																	}
 		hour++;
 	}
 }
