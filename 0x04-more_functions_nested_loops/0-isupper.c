dc3d026a59fdce822e3973c554d56b7ea4beff0e
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-14 05:49:37 -0700

    check upper charaters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,12 @@
 #include "main.h"
 /**
- * main - check the code.
+ * _isupper - checks if input integer's ascii value is uppercase
+ * @c: input number to check if it's ascii value is uppercase
  *
- * Return: Always 0.
+ * Return: 1 if upper, 0 if not
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-	{
-	
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c > 64 && c < 91 ? 1 : 0);
 }
+
