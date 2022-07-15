578bd675383d576a919cd407fda2ad48272933f2
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-15 13:20:25 -0700

    pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,10 +6,9 @@
  * @n: bytes to be copied from source string
  * 
  * Return: destination string concatenated
- *
  */
- char *_strncpy(char *dest, char *src, int n)
- {
+char *_strncpy(char *dest, char *src, int n)
+{
  	int j;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
