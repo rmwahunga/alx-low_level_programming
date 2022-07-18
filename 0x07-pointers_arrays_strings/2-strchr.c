ea58c100ed977409afa355c98c3a6d96449290a8
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-18 05:16:18 -0700

    character location

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ *
+ * @c: character to locate in the string
+ *
+ * @s: the string to check for the character
+ *
+ * Return: pointer to first instance of c, else return null
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (NULL);
+}
