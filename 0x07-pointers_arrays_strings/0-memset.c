0f0627d768da549d922544c1c3c109f9e1715da4
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-07-18 03:22:01 -0700

    memry bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * memeset-fills n bytes of memory
+ * _memset-fills n bytes of memory
  * @s:pointer to start of memory
  * @b: constant byte to fill with
  * @n: number of bytes to fill in
