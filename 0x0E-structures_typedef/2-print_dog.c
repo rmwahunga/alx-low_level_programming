905c394ea33f46466dc7dae3e02f505b24ca2754
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-08-02 01:16:21 -0700

    structures

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,15 +8,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d)
+	{
+		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
+	}
 }
