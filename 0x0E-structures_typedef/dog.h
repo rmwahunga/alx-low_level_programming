2a2af96704c92425082d85437d6d2e62e4654502
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-08-02 00:51:31 -0700

    strutures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
