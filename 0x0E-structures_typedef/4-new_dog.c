b543ffeacef20c56079bbce7f5923aa506862798
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-08-02 01:44:38 -0700

    structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,47 +1,65 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
-int get_len(int i);
-char *str_cpy(char *dest, char *src);
+#include <stdlib.h>
 /**
- * new_dog - a function that creates a new dog
- * get len of name + owner, malloc them, cpy name + owner to new
- * @name: name
- * @age: age
- * @owner: owner
- * Return: 0
+ * my_strlen - finds string length in order to malloc properly
+ * @str: input string
+ * Return: string length unsigned integer
+ */
+unsigned int my_strlen(char *str)
+{
+	unsigned int i = 0;
+
+	while (*(str + i) != '\0')
+		i++;
+	return (i);
+}
+/**
+ * my_strcpy - copies string
+ * @to: pointer to new string
+ * @from: pointer to string to copy
+ * Return: void
+ */
+void my_strcpy(char *to, char *from)
+{
+	unsigned int i, len = my_strlen(from);
+
+	for (i = 0; i <= len; i++)
+		to[i] = from[i];
+}
+/**
+ * new_dog - creates new type of struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: pointer to the type of struct dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_name;
-	char *copy_name, *copy_owner;
-	unsigned int x, name_len = 0, owner_len = 0;
+	unsigned int name_len = my_strlen(name), owner_len = my_strlen(owner);
+	dog_t *snoop_dogg;
 
-	new_name = malloc(sizeof(dog_t));
-		if (name == NULL)
-			return (NULL);
-		if (name == NULL || age <= 0 || owner == NULL)
+	snoop_dogg = malloc(sizeof(dog_t));
+	if (snoop_dogg)
+	{
+		snoop_dogg->name = malloc(sizeof(char) * (name_len + 1));
+		if (snoop_dogg->name)
 		{
-			free(new_name);
-				return (NULL);
+			snoop_dogg->owner = malloc(sizeof(char) * (owner_len + 1));
+			if (snoop_dogg->owner)
+			{
+				my_strcpy(snoop_dogg->name, name);
+				snoop_dogg->age = age;
+				my_strcpy(snoop_dogg->owner, owner);
+				return (snoop_dogg);
+			}
+			else
+			{
+			free(snoop_dogg->name);
+			free(snoop_dogg);
+			}
 		}
-
-		for (x = 0; name[x] != '\0'; x++)
-			name_len++;
-		for (x = 0; owner[x] != '\0'; x++)
-			owner_len++;
-		copy_name = malloc(sizeof(char) * (name_len + 1));
-		if (copy_name == NULL)
-			return (NULL)
-		copy_owner = malloc(sizeof(char) * (owner_len + 1));
-		if (copy_owner == NULL)
-			return (NULL);
-		for (x = 0; x <= name_len; x++)
-			copy_name[x] = name[x];
-		for (x = 0; x <= owner_len; x++)
-			copy_owner[x] = owner[x];
-		new_name->name = copy_name;
-		new_name->owner = copy_owner;
-		new_name->age = age;
-		return (new_name);
+		else
+			free(snoop_dogg);
+	}	
+	return (NULL);
 }
