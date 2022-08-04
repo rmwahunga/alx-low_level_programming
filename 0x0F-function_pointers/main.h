ea6a63811785ccdbb5696eca0cb1b2153a8dd62c
rmwahunga <richard.mwahunga@azubiafrica.org>
2022-08-04 02:57:45 -0700

    function pointers

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,7 +1,8 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
-
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+#include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
 #endif
